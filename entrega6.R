9150168b79db8ad388ea9d68e5b2e341ecdba498
MarcosOlivaMunoz <marcos.oliva1@estudiant.uib.cat>
2023-11-29 18:07:31 +0100

    Arreglos finales

--- entrega6.R
+++ entrega6.R
@@ -1,38 +1,39 @@
-datosPM <- read.table(file = "C:/Users/Hugo/Documents/Analisis_datos/Entrega_6_AD/PM.txt")
-datosSP <- read.table(file = "C:/Users/Hugo/Documents/Analisis_datos/Entrega_6_AD/SP.txt")
-datosVH <- read.table(file = "C:/Users/Hugo/Documents/Analisis_datos/Entrega_6_AD/VH.txt")
-phylum <- read.table(file = "C:/Users/Hugo/Documents/Analisis_datos/Entrega_6_AD/phylum.txt")
-
 library(magrittr)
 library(dplyr)
 library(ggpubr)
-phylum <- phylum %>% mutate(V2 = factor(V2,levels =c(101,102,103,104,105,106,107,108,109,110,111,112,113,114),labels=c("Vertebrates","Lancelet","Ascidians","Hechinoderms","Hemichordates","Arthropods","Nematodes","Annelids","Mollusks","Brachiopodas","Flatworms","Cnidarians","Placozoans","Poriferans")))
+
+PM <- read.table(file = "PM.txt")
+SP <- read.table(file = "SP.txt")
+VH <- read.table(file = "VH.txt")
+phylum <- read.table(file = "phylum.txt") %>%
+  mutate(V2 = factor(V2,levels =c(101,102,103,104,105,106,107,108,109,110,111,112,113,114),labels=c("Vertebrates","Lancelet","Ascidians","Hechinoderms","Hemichordates","Arthropods","Nematodes","Annelids","Mollusks","Brachiopodas","Flatworms","Cnidarians","Placozoans","Poriferans")))
 
 
-datosPM <- (1 - datosPM) %>%
+datosPM <- sqrt(2*(1 - PM)) %>%
   cmdscale() %>%
-  as_tibble()
-colnames(datosPM) <- c("Dim.1", "Dim.2")
- 
-datosPM2 <-bind_cols(datosPM,Phylum=phylum$V2)
+  as_tibble() %>% 
+  bind_cols(.,phylum$V2)
+colnames(datosPM) <- c("Dim.1", "Dim.2", "Phylum")
 
-ggscatter(datosPM2, x = "Dim.1", y = "Dim.2",color="Phylum",
-          size = 1)
+ggscatter(datosPM, x = "Dim.1", y = "Dim.2", color="Phylum",
+          size = 1) + labs(title = "Vertex Histogram")
 
 
-datosSP <- (1 - datosSP) %>%
+datosSP <- sqrt(2*(1 - SP)) %>%
   cmdscale() %>%
-  as_tibble()
-colnames(datosSP) <- c("Dim.1", "Dim.2")
-datosSP2 <-bind_cols(datosSP,Phylum=phylum$V2)
-ggscatter(datosSP2, x = "Dim.1", y = "Dim.2",color = "Phylum",
-          size = 1)
+  as_tibble() %>% 
+  bind_cols(.,phylum$V2)
+colnames(datosSP) <- c("Dim.1", "Dim.2", "Phylum")
 
-datosVH <- (1 - datosVH) %>%
+ggscatter(datosSP, x = "Dim.1", y = "Dim.2", color = "Phylum",
+          size = 1) + labs(title = "Shortest-Path")
+
+
+datosVH <- sqrt(2*(1 - VH)) %>%
   cmdscale() %>%
-  as_tibble()
-colnames(datosVH) <- c("Dim.1", "Dim.2")
-datosVH2 <-bind_cols(datosVH,Phylum=phylum$V2)
-ggscatter(datosVH2, x = "Dim.1", y = "Dim.2", color = "Phylum",
-          size = 1)
+  as_tibble() %>% 
+  bind_cols(.,phylum$V2)
+colnames(datosVH) <- c("Dim.1", "Dim.2", "Phylum")
 
+ggscatter(datosVH, x = "Dim.1", y = "Dim.2", color = "Phylum",
+          size = 1) + labs(title = "Pyramid Match")
